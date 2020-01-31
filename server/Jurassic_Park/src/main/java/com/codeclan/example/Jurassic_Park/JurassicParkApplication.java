31189747e626e0daaaba0f527a5d1e45d8f181c3
Iain <iain2eyes@gmail.com>
2020-01-31 15:21:58 +0000

    Projects initialised

---
+++ server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/JurassicParkApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Jurassic_Park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicParkApplication.class, args);
+	}
+
+}
---
+++ server/Jurassic_Park/src/test/java/com/codeclan/example/Jurassic_Park/JurassicParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Jurassic_Park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurassicParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
