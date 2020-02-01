b84bda7d13b48b0bf3ea12d76e01fb675c01c490
Talbekk <mt.campbell@outlook.com>
2020-02-01 19:49:25 +0000

    adds game controller

--- server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/components/DataLoader.java
+++ server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/components/DataLoader.java
@@ -1,4 +1,27 @@
 package com.codeclan.example.Jurassic_Park.components;
 
-public class DataLoader {
+
+import com.codeclan.example.Jurassic_Park.Models.Bank;
+import com.codeclan.example.Jurassic_Park.repository.banks.BankRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    BankRepository bankRepository;
+
+    public DataLoader() {
+
+    }
+
+    public void run(ApplicationArguments args){
+        Bank bank1 = new Bank(1000);
+        bankRepository.save(bank1);
+
+
+    }
 }
