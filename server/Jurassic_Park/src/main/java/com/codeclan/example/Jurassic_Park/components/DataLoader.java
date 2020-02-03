a312827e3b431dea8d23ecb6b9a137c52b23081e
Talbekk <mt.campbell@outlook.com>
2020-02-03 11:54:50 +0000

    refactors trex class to dinosaur

--- server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/components/DataLoader.java
+++ server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/components/DataLoader.java
@@ -1,10 +1,13 @@
 package com.codeclan.example.Jurassic_Park.components;
 
+import com.codeclan.example.Jurassic_Park.controller.DinosaurController;
 import com.codeclan.example.Jurassic_Park.models.Paddock;
-import com.codeclan.example.Jurassic_Park.models.TRex;
+import com.codeclan.example.Jurassic_Park.models.Dinosaur;
 import com.codeclan.example.Jurassic_Park.models.Triceratops;
+import com.codeclan.example.Jurassic_Park.models.inheritance.Dino;
+import com.codeclan.example.Jurassic_Park.repositories.DinosaurRepository;
 import com.codeclan.example.Jurassic_Park.repositories.PaddockRepository;
-import com.codeclan.example.Jurassic_Park.repositories.TRexRepository;
+import com.codeclan.example.Jurassic_Park.repositories.DinosaurRepository;
 import com.codeclan.example.Jurassic_Park.repositories.TriceratopsRepository;
 import com.codeclan.example.Jurassic_Park.repositories.inheritance.TriceratopsInheritRepository;
 
@@ -23,7 +26,7 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
-    TRexRepository tRexRepository;
+    DinosaurRepository dinosaurRepository;
 
     @Autowired
     PaddockRepository paddockRepository;
@@ -31,8 +34,8 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     TriceratopsRepository triceratopsRepository;
 
-    @Autowired
-    TriceratopsInheritRepository triceratopsInheritRepository;
+//    @Autowired
+//    TriceratopsInheritRepository triceratopsInheritRepository;
 
     @Autowired
     BankRepository bankRepository;
@@ -57,13 +60,13 @@ public class DataLoader implements ApplicationRunner {
         Paddock paddock = new Paddock("West Paddock", 1, 3, 4, false, game1);
         paddockRepository.save(paddock);
 
-        TRex tRex = new TRex(4, 5, "cow", 5, paddock);
-        tRexRepository.save(tRex);
-        TRex tRex2 = new TRex(10, 5, "cow", 5, paddock);
-        tRexRepository.save(tRex2);
+        Dinosaur tRex = new Dinosaur("T-Rex",4, 5, "carnivore", 5, paddock);
+        dinosaurRepository.save(tRex);
+        Dinosaur tRex2 = new Dinosaur("Triceratops", 10, 5, "herbivore", 5, paddock);
+        dinosaurRepository.save(tRex2);
 
-        Triceratops triceratops = new Triceratops(4, 5, "shrubbery", 5, paddock);
-        triceratopsRepository.save(triceratops);
+//        Triceratops triceratops = new Triceratops(4, 5, "herbivore", 5, paddock);
+//        triceratopsRepository.save(triceratops);
 
 //      TriceratopsInherit triceratops = new TriceratopsInherit(10, 30, "Shrubbery", 3, paddock);
 //      triceratopsInheritRepository.save(triceratops);
