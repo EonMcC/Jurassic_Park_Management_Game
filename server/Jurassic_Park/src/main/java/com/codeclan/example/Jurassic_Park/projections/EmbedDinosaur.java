b4d25d5f8086b83e9e2ab6db5f23170b201097b4
Talbekk <mt.campbell@outlook.com>
2020-02-03 12:37:24 +0000

    adds embedded info to dino routes

--- server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/projections/EmbedDinosaur.java
+++ server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/projections/EmbedDinosaur.java
@@ -1,7 +1,11 @@
 package com.codeclan.example.Jurassic_Park.projections;
 
+import com.codeclan.example.Jurassic_Park.models.Dinosaur;
+import com.codeclan.example.Jurassic_Park.models.Game;
 import com.codeclan.example.Jurassic_Park.models.Paddock;
 import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
 
 @Projection(name="embedDinosaur", types = Paddock.class)
 public interface EmbedDinosaur {
@@ -12,4 +16,6 @@ public interface EmbedDinosaur {
     int getCostToBuy();
     int getUpKeepCost();
     boolean isOwned();
+    List<Dinosaur> getDinosaurs();
+    Game getGame();
 }
