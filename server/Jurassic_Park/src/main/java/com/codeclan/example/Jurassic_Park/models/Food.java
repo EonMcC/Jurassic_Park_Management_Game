8cbcd71782bd5dda6dbbac1185902b81812bcf78
Talbekk <mt.campbell@outlook.com>
2020-02-03 12:08:39 +0000

    creates food class

---
+++ server/Jurassic_Park/src/main/java/com/codeclan/example/Jurassic_Park/models/Food.java
@@ -0,0 +1,71 @@
+package com.codeclan.example.Jurassic_Park.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="foods")
+public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="price")
+    private int price;
+
+    @Column(name="replen_level")
+    private int replenLevel;
+
+    @Column(name="type")
+    private String type;
+
+    public Food(String name, int price, int replenLevel, String type) {
+        this.name = name;
+        this.price = price;
+        this.replenLevel = replenLevel;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getReplenLevel() {
+        return replenLevel;
+    }
+
+    public void setReplenLevel(int replenLevel) {
+        this.replenLevel = replenLevel;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
